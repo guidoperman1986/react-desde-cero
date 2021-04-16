dd5ee3ddd3e340e2298e64906b8fa495fd4c9b41
Guido Perman <guido.perman@globant.com>
2021-04-16 15:24:53 -0300

    added npm, eslint, prettier

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-const Pet = ({name ,animal ,breed}) => {
+const Pet = ({ name, animal, breed }) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, name),
     React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
+    React.createElement("h2", {}, breed),
   ]);
 };
 
