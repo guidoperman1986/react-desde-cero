26d042a080d5ee2932292041ea2089717708e9e0
Guido Perman <guido.perman@globant.com>
2021-04-18 21:02:47 -0300

    react useState hook and custom hook

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import React from 'react';
 import {render} from 'react-dom';
-import Pet from './Pet';
+/* import Pet from './Pet'; */
+import SearchParams from './SearchParams';
 
 const App = () => {
-  /* return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-  ]); */
-
   return (
     <div>
-      <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese"></Pet>
-      <Pet name="Luna" animal="Dog" breed="Havanese"></Pet>
-      <Pet name="Luna" animal="Dog" breed="Havanese"></Pet>
+      <h1>Adopt me!</h1>
+      <SearchParams />
     </div>
   );
 };
