75666c2dd97807372f4ebdc1c4acbfd9be3814b5
Guido <38866056+guidoperman1986@users.noreply.github.com>
2021-05-18 22:54:32 -0300

    context

--- src/Details.js
+++ src/Details.js
@@ -1,45 +1,50 @@
 import React from "react";
-import pet from '@frontendmasters/pet';
-import Carrousel from './Carrousel';
+import pet from "@frontendmasters/pet";
+import Carrousel from "./Carrousel";
 import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
 
-class Details extends React.Component{
-  state = {loading: true};
+class Details extends React.Component {
+  state = { loading: true };
 
-  componentDidMount(){
-    pet.animal(this.props.id)
-        .then(({animal}) =>{
-          this.setState({
-            name: animal.name,
-            animal: animal.type,
-            location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
-            description: animal.description,
-            media: animal.photos,
-            breed: animal.breeds.primary,
-            loading: false
-          })
-        }, console.error); 
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false,
+      });
+    }, console.error);
   }
 
-  render () {
-      if (this.state.loading){
-        return <h1>Loading...</h1>
-      }
+  render() {
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
 
-      const {animal, breed, location, description, name, media} = this.state;
+    const { animal, breed, location, description, name, media } = this.state;
 
-      return (        
-        <div className="details">
-          <Carrousel media={media}/>
-          <div>
-            <h1>{name}</h1>
-            <h2>{`${animal} - ${breed} - ${location}`}</h2>
-            <button>Adopt {name}</button>
-            <p>{description}</p>
-          </div>
+    return (
+      <div className="details">
+        <Carrousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button style={{ backgroundColor: theme }}>
+                Adopt {name}
+              </button>
+            )}
+          </ThemeContext.Consumer>
+          <p>{description}</p>
         </div>
-      );
-
+      </div>
+    );
   }
 }
 
